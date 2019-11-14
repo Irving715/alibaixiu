b62579f992421f59ec7fb453655f886997a06c28
irving <1250648985@qq.com>
2019-11-14 15:32:53 +0800

    退出bug

---
+++ public/assets/js/common.js
@@ -0,0 +1,18 @@
+$('#logout').on('click', function () {
+    alert('  被点击了')
+    var isConfirm = confirm('您真的要退出吗');
+    if (isConfirm) {
+        $.ajax({
+            type: "post",
+            url: "/logout",
+            success: function () {
+                location.href = 'login.html'
+                // location.assign('index.html')
+                // location.replace('login.html');
+            },
+            error: function () {
+                alert('退出登录失败')
+            }
+        });
+    }
+})
