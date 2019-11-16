82f3939b0585a1bd6d1bd872a4ec86cb8a75a9be
irving <1250648985@qq.com>
2019-11-16 15:09:07 +0800

    修改文章列表分类未实现

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -21,3 +21,31 @@ $.ajax({
         $('#categoryBox').html(html);
     }
 });
+// 事件委托  要写谁触发点击事件
+$('#categoryBox').on('click', '.edit', function () {
+    var id = $(this).attr('data-id');
+
+    $.ajax({
+        type: "get",
+        url: "/categories/" + id,
+        success: function (response) {
+            console.log(response);
+            let html = template('modifyCategoryTpl', response);
+            $('#formBox').html(html);
+        }
+    });
+});
+$('#formBox').on('submit', 'modifyCategory', function () {
+    let formData = $(this).serialize();
+    let id = $(this).attr('data-id');
+
+    $.ajax({
+        type: "put",
+        url: "/categories/" + id,
+        data: formData,
+        success: function () {
+            location.reload()
+        }
+    });
+    return false;
+})
