79f859196a3afab577b5e82817717ee5d3c8fc22
irving <1250648985@qq.com>
2019-11-16 19:12:12 +0800

    分页功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -3,14 +3,31 @@ $.ajax({
     url: "/posts",
     success: function (response) {
         console.log(response);
-        let html = template('postsTpl', response)
+        let html = template('postsTpl', response);
         $('#postsBox').html(html);
-
-
+        var page = template('pageTpl', response);
+        $('#page').html(page);
     }
 });
+
+
 // 处理时间格式
 function formateDate(date) {
     date = new Date(date);
     return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+};
+
+function changePage(page) {
+    $.ajax({
+        type: "get",
+        url: "/posts",
+        data: {
+            page
+        },
+        success: function (response) {
+            let html = template('postsTpl', response);
+            $('#postsBox').html(html);
+            let page = template('pageTpl', response);
+        }
+    });
 }
