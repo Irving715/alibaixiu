bfb4c23be007bb498ab67128d6d12e95d9b066cc
irving <1250648985@qq.com>
2019-11-16 20:55:23 +0800

    上一页按钮bug

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -22,12 +22,42 @@ function changePage(page) {
         type: "get",
         url: "/posts",
         data: {
-            page
+            page: page
         },
         success: function (response) {
             let html = template('postsTpl', response);
             $('#postsBox').html(html);
             let page = template('pageTpl', response);
+
         }
     });
-}
+};
+console.log(page);
+
+$.ajax({
+    type: "get",
+    url: "/categories",
+    success: function (response) {
+        console.log(response);
+        let html = template('categoryTpl', {
+            data: response
+        });
+        $('#categoryBox').html(html);
+    }
+});
+$('#filterForm').on('submit', function () {
+    let formData = $(this).serialize();
+
+    $.ajax({
+        type: "get",
+        url: "/posts",
+        data: formData,
+        success: function (response) {
+            let html = template('postsTpl', response);
+            $('#postsBox').html(html);
+            let page = template('pageTpl', response);
+            $('#page').html(page);
+        }
+    });
+    return false;
+})
